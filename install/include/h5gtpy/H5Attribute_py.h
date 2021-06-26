ca69f850ada729b8751802dceb84a68ead239862
Kerim <kerim.khemraev@mail.ru>
2021-06-26 16:31:58 +0300

    typo: remove `install` dirs from package

--- install/include/h5gtpy/H5Attribute_py.h
+++
@@ -1,8 +0,0 @@
-#include "h5gt_py.h"
-
-namespace h5gtpy {
-
-void Attribute_py(py::class_<Attribute, Object> &py_obj);
-
-
-} // h5gtpy
--- install/include/h5gtpy/H5File_py.h
+++
@@ -1,8 +0,0 @@
-#include "h5gt_py.h"
-
-namespace h5gtpy {
-
-void File_py(py::class_<File, Object, NodeTraits<File>, AnnotateTraits<File> > &py_obj);
-
-
-} // h5gtpy
